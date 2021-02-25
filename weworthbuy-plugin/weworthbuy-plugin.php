92551e466026e1262ea61313b15902e88793789f
Jiean Yang <jiean.yang8534@gmail.com>
2021-02-25 23:58:28 +0100

    Add setting links

--- weworthbuy-plugin/weworthbuy-plugin.php
+++ weworthbuy-plugin/weworthbuy-plugin.php
@@ -24,10 +24,26 @@
 	class WeworthbuyPlugin 
 	{
 
+		public $plugin;
+
+		function __construct() {
+			$this->plugin = plugin_basename(__FILE__);
+		}
+
 		function register() {
 			add_action('admin_enqueue_scripts', array($this, 'enqueue'));
 
 			add_action('admin_menu', array($this, 'add_admin_pages'));
+
+			add_filter("plugin_action_links_$this->plugin", array($this, 'settings_link'));
+		}
+
+
+		public function settings_link($links) {
+			$settings_link = '<a href="admin.php?page=weworthbuy_plugin">Settings</a>';
+			array_push($links, $settings_link);
+
+			return $links;
 		}
 
 
