97eb35e2f3d966db13727007a77e88717f634f97
Jiean Yang <jiean.yang8534@gmail.com>
2021-03-16 22:34:00 +0100

    Develop Init.php file and update Admin.php

--- weworthbuy-plugin/inc/Init.php
+++ weworthbuy-plugin/inc/Init.php
@@ -6,10 +6,42 @@ namespace Inc;
 
 
 
-class Init
+// Cannot extend Init --> final keyword
+final class Init
 {
+  /**
+   * Store all the classes inside an array
+   * @return array Full list of classes
+   */
+  public static function get_services() {
+    return [
+      Pages\Admin::class
+    ];
+  }
+
+  /**
+   * Loop through the classes, initialize them, 
+   * and call the register() method if it exists
+   * @return
+   */
 	public static function register_services() {
-    // construct
+    foreach(self::get_services() as $class) {
+      $service = self::instantiate($class);
+      if(method_exists($service, 'register')) {
+        $service->register();
+      }
+    }
+  }
+
+  /**
+   * Initialize the class
+   * @param class $class      class from the services array
+   * @return class instance   new instance of the class
+   */
+  private static function instantiate($class)
+  {
+    $service = new $class();
+    return $service;
   }
 }
 
