341973fe051f576ce1379444830ce9d797b3d944
Jiean Yang <jiean.yang8534@gmail.com>
2021-03-19 10:36:37 +0100

    Optimize starter-plugin-structure

--- weworthbuy-plugin/weworthbuy-plugin.php
+++ weworthbuy-plugin/weworthbuy-plugin.php
@@ -24,29 +24,20 @@ if(file_exists(dirname(__FILE__) . '/vendor/autoload.php')) {
 	require_once dirname(__FILE__) . '/vendor/autoload.php';
 }
 
-// Define CONSTANTS
-define('PLUGIN_PATH', plugin_dir_path(__FILE__));
-define('PLUGIN_URL', plugin_dir_url(__FILE__));
-define('PLUGIN', plugin_basename(__FILE__));
-
-use Inc\Base\Activate;
-use Inc\Base\Deactivate;
-
 /**
  * The code that runs during plugin activate
  */
 function activate_weworthbuy_plugin() {
-	Activate::activate();
+	Inc\Base\Activate::activate();
 }
+register_activation_hook(__FILE__, 'activate_weworthbuy_plugin');
 
 /**
  * The code that runs during plugin deactivate
  */
 function deactivate_weworthbuy_plugin() {
-	Deactivate::deactivate();
+	Inc\Base\Deactivate::deactivate();
 }
-
-register_activation_hook(__FILE__, 'activate_weworthbuy_plugin');
 register_deactivation_hook(__FILE__, 'deactivate_weworthbuy_plugin');
 
 /**
